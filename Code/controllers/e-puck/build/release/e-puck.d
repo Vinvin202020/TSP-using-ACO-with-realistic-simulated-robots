1318f54ec1e06aa6e3f85bcf130f00ca35332f71
EmilieHerbault <emilie.herbault@epfl.ch>
2025-11-23 17:53:00 +0100

    Color
    Les couleurs marchent et j'ai rajouté des ligths

--- Code/controllers/e-puck/build/release/e-puck.d
+++ Code/controllers/e-puck/build/release/e-puck.d
@@ -7,4 +7,6 @@ build/release/e-puck.o: e-puck.c \
  /usr/local/webots-2022a/include/controller/c/webots/inertial_unit.h \
  /usr/local/webots-2022a/include/controller/c/webots/distance_sensor.h \
  /usr/local/webots-2022a/include/controller/c/webots/emitter.h \
- /usr/local/webots-2022a/include/controller/c/webots/receiver.h
+ /usr/local/webots-2022a/include/controller/c/webots/receiver.h \
+ /usr/local/webots-2022a/include/controller/c/webots/camera.h \
+ /usr/local/webots-2022a/include/controller/c/webots/camera_recognition_object.h
