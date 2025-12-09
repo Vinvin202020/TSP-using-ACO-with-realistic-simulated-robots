5b981c1ddfe2c48348671d2127fe67fe4a2e35fb
esalathe <119804528+esalathe@users.noreply.github.com>
2025-12-09 20:04:42 +0100

    Delete Code_partie2 directory

--- Code_partie2/controllers/supervisor/build/release/supervisor.d
+++
@@ -1,8 +0,0 @@
-build/release/supervisor.o: supervisor.c \
- /usr/local/webots-2022a/include/controller/c/webots/robot.h \
- /usr/local/webots-2022a/include/controller/c/webots/types.h \
- /usr/local/webots-2022a/include/controller/c/webots/nodes.h \
- /usr/local/webots-2022a/include/controller/c/webots/supervisor.h \
- /usr/local/webots-2022a/include/controller/c/webots/contact_point.h \
- /usr/local/webots-2022a/include/controller/c/webots/emitter.h \
- /usr/local/webots-2022a/include/controller/c/webots/receiver.h
